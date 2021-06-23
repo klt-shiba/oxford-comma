774543e9b5b48cfd6f075061c98fad46bdb43f31
Colin Tseng <klt.aus@yahoo.com>
2021-06-23 18:48:24 +1000

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
 
+    if array.length() == 2
+
+        array.join(" and ")
+    
+    elsif array.length() >=3
+
+        lastItem = array.last()   
+        newResults = array
+
+        newResults.pop()
+        newResults.join(", ")
+        newResults << "and #{lastItem}"
+        newResults.join(", ")
+        
+        
+    else
+
+        array.join
+    
+    end
 end
